2d6d88b8623e89881072e8fd81e3ec7c86ea4eaf
Akbar Rizki Maulana <akbarezeki@gmail.com>
2021-07-26 17:31:09 +0700

    fix deploy script

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -5,15 +5,16 @@ sudo docker pull akbarezeki/taskaction:dev-${{ steps.tagstep.outputs.today }}-${
 
 # Check for running container 
 if [ ! $(docker ps -q -f name=taskactioncontainer) ]; then
-    # Run Container
+    # Run container
     sudo docker run -d -p 8000:80 -p 443:443 \
         -v /etc/mnt/index.html:/usr/share/nginx/html \
         -v /etc/mnt/nginx.conf:/usr/nginx \
         -v /etc/mnt:/usr/nginx/ssl \
         --name taskactioncontainer akbarezeki/taskaction:dev-${{ steps.tagstep.outputs.today }}-${{ steps.tagstep.outputs.commitsh }}
 else
+    #Clean up then run container
     sudo docker rm -f taskactioncontainer
-    
+
     sudo docker run -d -p 8000:80 -p 443:443 \
         -v /etc/mnt/index.html:/usr/share/nginx/html \
         -v /etc/mnt/nginx.conf:/usr/nginx \
