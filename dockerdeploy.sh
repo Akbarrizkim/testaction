c679955bdbc6e4518120f3f5d20760670825de26
Akbar Rizki Maulana <akbarezeki@gmail.com>
2021-07-26 19:05:02 +0700

    update Conditional check to include created and exited containers not running

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -8,6 +8,9 @@ sudo docker pull akbarezeki/taskaction:dev-$IMAGEDATE-$IMAGECOMMITSH
 
 # Check for running container 
 if [ ! $(sudo docker ps -q -f name=taskactioncontainer) ]; then
+    if [ $(sudo docker ps -aq -f status=exited -f name=taskactioncontainer) ] || [ $(sudo docker ps -aq -f status=exited -f name=taskactioncontainer) ]; then
+        sudo docker rm -f taskactioncontainer
+    fi
     # Run container
     sudo docker run -d -p 8000:80 -p 443:443 \
         -v /etc/mnt/index.html:/usr/share/nginx/html \
