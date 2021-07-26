50b1e444e526ae648b3c643299c2828ab06a74d1
Akbar Rizki Maulana <akbarezeki@gmail.com>
2021-07-26 19:10:50 +0700

    fix typo in docker run command

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -8,20 +8,20 @@ sudo docker pull akbarezeki/taskaction:dev-$IMAGEDATE-$IMAGECOMMITSH
 
 # Check for running container 
 if [ ! $(sudo docker ps -q -f name=taskactioncontainer) ]; then
-    if [ $(sudo docker ps -aq -f status=exited -f name=taskactioncontainer) ] || [ $(sudo docker ps -aq -f status=exited -f name=taskactioncontainer) ]; then
+    if [ $(sudo docker ps -aq -f status=exited -f name=taskactioncontainer) ] || [ $(sudo docker ps -aq -f status=created -f name=taskactioncontainer) ]; then
         sudo docker rm -f taskactioncontainer
     fi
     # Run container
     sudo docker run -d -p 8000:80 -p 443:443 \
-        -v /etc/mnt/index.html:/usr/share/nginx/html \
-        -v /etc/mnt:/usr/nginx \
+        -v /etc/mnt/index.html:/usr/share/nginx/html/ \
+        -v /etc/mnt/:/usr/nginx/ \
         --name taskactioncontainer akbarezeki/taskaction:dev-$IMAGEDATE-$IMAGECOMMITSH
 else
     #Clean up then run container
     sudo docker rm -f taskactioncontainer
 
     sudo docker run -d -p 8000:80 -p 443:443 \
-        -v /etc/mnt/index.html:/usr/share/nginx/html \
-        -v /etc/mnt:/usr/nginx \
+        -v /etc/mnt/index.html:/usr/share/nginx/html/ \
+        -v /etc/mnt/:/usr/nginx/ \
         --name taskactioncontainer akbarezeki/taskaction:dev-$IMAGEDATE-$IMAGECOMMITSH
 fi
