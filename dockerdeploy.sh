02de0f0ba992728ac47886a92669447db0a5ef57
Akbar Rizki Maulana <akbarezeki@gmail.com>
2021-07-26 19:19:53 +0700

    fix run command to just use dir path not exact file path

--- dockerdeploy.sh
+++ dockerdeploy.sh
@@ -13,15 +13,15 @@ if [ ! $(sudo docker ps -q -f name=taskactioncontainer) ]; then
     fi
     # Run container
     sudo docker run -d -p 8000:80 -p 443:443 \
-        -v /etc/mnt/index.html:/usr/share/nginx/html/ \
-        -v /etc/mnt/:/usr/nginx/ \
+        -v /etc/mnt:/usr/share/nginx/html \
+        -v /etc/mnt:/usr/nginx \
         --name taskactioncontainer akbarezeki/taskaction:dev-$IMAGEDATE-$IMAGECOMMITSH
 else
     #Clean up then run container
     sudo docker rm -f taskactioncontainer
 
     sudo docker run -d -p 8000:80 -p 443:443 \
-        -v /etc/mnt/index.html:/usr/share/nginx/html/ \
-        -v /etc/mnt/:/usr/nginx/ \
+        -v /etc/mnt:/usr/share/nginx/html \
+        -v /etc/mnt:/usr/nginx \
         --name taskactioncontainer akbarezeki/taskaction:dev-$IMAGEDATE-$IMAGECOMMITSH
 fi
